73e2b3f81a73ba506037eac7a784aef1416cb59a
codehz <git@hertz.moe>
2023-12-19 23:20:24 +0800

    add methods to string

--- $base/String.h
+++ $base/String.h
@@ -2,12 +2,25 @@
 
 #import "./Object.h"
 
+#define FORMAT ... __attribute__((format(printf, 1, 2)))
+
 @interface $String : $Object {
-  const char *data;
+  char *data;
   unsigned int length;
 }
 
-@property(readonly) const char *data;
-@property(readonly) unsigned int length;
+@property(nonatomic, readonly) const char *data;
+@property(nonatomic, readonly) char *unsafe_data;
+@property(nonatomic, readonly) unsigned int length;
+
++ (instancetype)$copyFrom:(const char *)data length:(unsigned int)length;
++ (instancetype)$allocWithLength:(unsigned int)length;
++ (instancetype)$createFromCString:(char *)data;
++ (instancetype)$format:(const char *)format, FORMAT;
+
+/** # DON'T DO IT FOR CONSTANTS */
+- (void)$dealloc;
 
 @end
+
+#undef FORMAT
--- String.m
+++ String.m
@@ -1,8 +1,48 @@
 #import "$base/String.h"
+#import <stdio.h>
+#import <stdlib.h>
 
 @implementation $String
 
-@synthesize data;
+@dynamic data;
+@synthesize unsafe_data = data;
 @synthesize length;
+
+- (const char *)data {
+  return (const char *)data;
+}
+
++ (instancetype)$copyFrom:(const char *)data length:(unsigned int)length {
+  $String *ret = [self $alloc];
+  ret->data = malloc(length);
+  memcpy(ret->data, data, length);
+  ret->length = length;
+  return ret;
+}
++ (instancetype)$allocWithLength:(unsigned int)length {
+  $String *ret = [self $alloc];
+  ret->data = malloc(length);
+  return ret;
+}
++ (instancetype)$createFromCString:(char *)data {
+  $String *ret = [self $alloc];
+  ret->data = data;
+  ret->length = strlen(data);
+  return ret;
+}
+
++ (instancetype)$format:(const char *)format, ... {
+  $String *ret = [self $alloc];
+  va_list list;
+  va_start(list, format);
+  ret->length = vasprintf(&ret->data, format, list);
+  va_end(list);
+  return ret;
+}
+
+- (void)$dealloc {
+  free((void *)data);
+  [super $dealloc];
+}
 
 @end
